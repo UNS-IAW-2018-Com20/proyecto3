73c74768fa6c6a91d0cb6a53aac4bcf43affafb4
jorge <jorgecardozosilvio@gmail.com>
2018-06-09 20:09:26 -0300

    saint seya!

--- resources/views/crearEscala.blade.php
+++ resources/views/crearEscala.blade.php
@@ -20,27 +20,28 @@
 
                     <div class="myform-bottom">
 
-                      <h1>Cantidad de Notas de la Escala</h1>
-                      <input type="number" id="cantNotas" min="2" max="10" value="2" onChange="mostrarNotas()" />
+
                       <h1>Notas</h1>
                       <form method="post">
                       {{ csrf_field() }}
                       Descripcion de la Escala:
-                      <input type="text" name="descripcion" required="true"/>
+                      <input class="form-group form-control" type="text" name="descripcion" required="true"/>
 
-                      <div id="notasEscalaContainer">
+                      <div class="form-group" id="notasEscalaContainer">
+                        <h1>Cantidad de Notas de la Escala</h1>
+                        <input class="form-control" type="number" id="cantNotas" min="2" max="10" value="2" onChange="mostrarNotas()" />
                         @for ($i = 0; $i < 2; $i++)
                           Nota:
-                          <input type="text" name="notas[]" required="true"/>
+                          <input class="form-control form-group" type="text" name="notas[]" required="true"/>
                           Concepto:
-                          <select name="conceptos[]">
+                          <select class="form-control" name="conceptos[]">
                             <option>Aprobado</option>
                             <option>Desaprobado</option>
                           </select> <br/>
                         @endfor
                       </div>
 
-                      <input type="submit" />
+                      <input type="submit" class="btn btn-primary"/>
                     </form>
                     </div>
 
