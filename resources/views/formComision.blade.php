b92d6bbd94d1d407803d3c8413e4cf4404d260cb
Juan <juan.manuel.iglesias93@gmail.com>
2018-06-10 20:51:23 -0300

    Cambio de form comision

--- resources/views/formComision.blade.php
+++ resources/views/formComision.blade.php
@@ -17,7 +17,7 @@
                       <div class="myform-bottom">
 
 
-                        <form method="post form-control">
+                        <form method="post">
                           {{ csrf_field() }}
                           <div class="form-group">
                             Nombre de la Comisión
